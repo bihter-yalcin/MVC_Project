efc8251ce35388e1d7ff0b20beddd915ba405f04
bihter.yalcin <bihter.yalcin@BIHTERYALCIN>
2021-03-23 15:02:58 +0300

    onedoteighteen

---
+++ MVC_Application/Models/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Application.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+
+        [DisplayName("Category Name")]
+        public string CategoryName { get; set; }
+
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order should be greater than 0")]
+        public int DisplayOrder { get; set; }
+    }
+}
